43fdada07171fad2bef8da058091165b8eeb4516
doggySummer <matthew9304@gmail.com>
2025-02-25 20:34:34 +0900

    hello world 🤚

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+
+
+const index = () => {
+    return (
+        <View>
+            <Text>hello world</Text>
+        </View>
+    )
+}
+
+export default index
