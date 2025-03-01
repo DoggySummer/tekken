5322bef57e9e51a9c2172391a60cfbe897df25ba
doggySummer <matthew9304@gmail.com>
2025-03-01 12:22:57 +0900

    배너예시시

--- app/(tabs)/about/[character].tsx
+++ app/(tabs)/about/[character].tsx
@@ -1,17 +1,19 @@
-import { Text, SafeAreaView, StyleSheet } from 'react-native'
+import { Text, SafeAreaView, StyleSheet, Image } from 'react-native'
 import { useLocalSearchParams } from 'expo-router'
 import Banner from '@/components/banner'
 import { characterList } from '@/constant/constant'
 import { colors } from '@/constant/colors'
 import Skill from '@/components/skill'
 import { dummyskill01, dummyskill02 } from '@/constant/constant'
+import bannerExample from '@/assets/images/bannerExample.png'
 
 export default function Index() {
   const { character } = useLocalSearchParams()
   const characterData = characterList.find((item) => item.name === character)
   return (
     <SafeAreaView style={style.container}>
-      <Banner image={characterData?.image} />
+      <Image source={bannerExample} style={style.banner} />
+      {/* <Banner image={characterData?.image} /> */}
       <Skill {...dummyskill01} />
       {/* <Skill {...dummyskill02} /> */}
     </SafeAreaView>
@@ -22,5 +24,9 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.BGCOLOR,
+  },
+  banner: {
+    width: '100%',
+    objectFit: 'contain',
   },
 })
